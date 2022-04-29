de8d2d7287462780ab2ebb80c4b8b959b9a68c3c
Vladimir Camagic <vladimir.camagic@vegait.rs>
2022-04-29 13:46:28 +0200

    commit

--- Entities/RequestFeatures/RequestFeatures.cs
+++ Entities/RequestFeatures/RequestFeatures.cs
@@ -20,10 +20,16 @@ public abstract class RequestParameters
 
 public class EmployeeParameters : RequestParameters
 {
+    public EmployeeParameters()
+    {
+        OrderBy = "name";
+    }
     public uint MinAge { get; set; }
     public uint MaxAge { get; set; } = int.MaxValue;
 
     public bool ValidAgeRange => MaxAge > MinAge;
 
     public string SearchTerm { get; set; }
+
+    public string OrderBy { get; set; }
 }
